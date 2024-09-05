b6b78d9fed76a743cccc1e53449285e26c387f55
19799 <1979905046@qq.com>
2024-09-05 21:03:23 +0800

    JavaNativeCodeSandBox类中实现： 1.将文件中的代码写入全局代码目录下，并且把不同用户的代码隔离存放 2.程序将用户代码编译 3.执行代码，得到结果 4.收集信息，包括每一条测试用例的执行时间（取最大的时间用于判断是否超时）程序执行过程的错误信息、程序正常执行的信息 5.程序执行结束，收集完信息后，自动清理产生的java文件和class文件。

--- src/main/java/com/bro/liojcodesandbox/JavaNativeCodeSandBox.java
+++ src/main/java/com/bro/liojcodesandbox/JavaNativeCodeSandBox.java
@@ -2,14 +2,21 @@ package com.bro.liojcodesandbox;
 
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.resource.ResourceUtil;
+import cn.hutool.core.util.StrUtil;
 import com.bro.liojcodesandbox.model.ExecuteCodeRequest;
 import com.bro.liojcodesandbox.model.ExecuteCodeResponse;
+import com.bro.liojcodesandbox.model.ExecuteMessage;
+import com.bro.liojcodesandbox.model.JudgeInfo;
+import com.bro.liojcodesandbox.utils.ProccessUtils;
+import org.springframework.util.StopWatch;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class JavaNativeCodeSandBox implements CodeSandBox {
 
@@ -21,8 +28,8 @@ public class JavaNativeCodeSandBox implements CodeSandBox {
     public static void main(String[] args) {
         JavaNativeCodeSandBox javaNativeCodeSandBox = new JavaNativeCodeSandBox();
         ExecuteCodeRequest executeCodeRequest = new ExecuteCodeRequest();
-        executeCodeRequest.setInputList(Arrays.asList("1 2,3 4"));
-        String code = ResourceUtil.readStr("testCode/simpleComputeArgs/SimpleCompute.java", StandardCharsets.UTF_8);
+        executeCodeRequest.setInputList(Arrays.asList("1 2","3 4"));
+        String code = ResourceUtil.readStr("testCode/simpleComputeArgs/Main.java", StandardCharsets.UTF_8);
         executeCodeRequest.setCode(code);
         executeCodeRequest.setLanguage("java");
         ExecuteCodeResponse executeCodeResponse = javaNativeCodeSandBox.executeCode(executeCodeRequest);
@@ -51,6 +58,85 @@ public class JavaNativeCodeSandBox implements CodeSandBox {
         String userCodePath = userCodeParentPath + File.separator + GLOBAL_JAVA_CLASS_NAME;
         File userCodeFile = FileUtil.writeString(code, userCodePath, StandardCharsets.UTF_8);
 
-        return null;
+        //编译代码，得到class文件
+        String compileCmd = String.format("javac -encoding utf-8 %s", userCodeFile.getAbsolutePath());
+        try {
+            Process compileProcess = Runtime.getRuntime().exec(compileCmd);
+            //等待程序执行，获取错误码
+            ExecuteMessage executeMessage = ProccessUtils.runProcessAndGetMessage(compileProcess, "编译");
+            System.out.println(executeMessage);
+        } catch (Exception e) {
+            return getResponse(e);
+        }
+
+
+        //执行代码，得到结果
+        List<ExecuteMessage> executeMessagesList =new ArrayList<>();
+        for(String inputArgs : inputList){
+            String runCmd=String.format("java -Dfile.encoding=UTF-8 -cp %s Main %s",userCodeParentPath,inputArgs);
+            try {
+                Process runProcess = Runtime.getRuntime().exec(runCmd);
+                ExecuteMessage executeMessage = ProccessUtils.runProcessAndGetMessage(runProcess, "运行");
+                //ExecuteMessage executeMessage=ProccessUtils.runInteractProcessAndGetMessage(runProcess,"运行",inputArgs);
+                System.out.println(executeMessage);
+                executeMessagesList.add(executeMessage);
+            } catch (Exception e) {
+                return getResponse(e);
+            }
+        }
+
+        //收集整理输出结果
+        ExecuteCodeResponse executeCodeResponse = new ExecuteCodeResponse();
+        List<String> outputList=new ArrayList<>();
+        //取用时最大值，便于判断是否超时
+        long maxTime=0;
+        for(ExecuteMessage executeMessage : executeMessagesList){
+            String errorMessage = executeMessage.getErrorMessage();
+            if(StrUtil.isNotBlank(errorMessage)){
+                executeCodeResponse.setMessage(errorMessage);
+                //执行中存在错误
+                executeCodeResponse.setStatus(3);
+                break;
+            }
+            outputList.add(executeMessage.getMessage());
+            Long time=executeMessage.getTime();
+            if(time!=null){
+                maxTime=Math.max(maxTime,time);
+            }
+        }
+        if(outputList.size()==executeMessagesList.size()){
+            //正常运行完成
+            executeCodeResponse.setStatus(1);
+        }
+        executeCodeResponse.setOutputList(outputList);
+        JudgeInfo judgeInfo =new JudgeInfo();
+        //judgeInfo.setMessage();
+        //需要借助第三方库来实现，此处不做实现
+        //judgeInfo.setMemory();
+        judgeInfo.setTime(maxTime);
+        executeCodeResponse.setJudgeInfo(judgeInfo);
+
+        //清理文件
+        if(userCodeFile.getParentFile()!=null){
+            boolean del = FileUtil.del(userCodeParentPath);
+            System.out.println("删除"+(del ? "成功":"失败"));
+        }
+        return executeCodeResponse;
+    }
+
+
+    /**
+     * 获取错误响应
+     * @param e
+     * @return
+     */
+    private ExecuteCodeResponse getResponse(Throwable e){
+        ExecuteCodeResponse executeCodeResponse = new ExecuteCodeResponse();
+        executeCodeResponse.setOutputList(new ArrayList<>());
+        executeCodeResponse.setMessage(e.getMessage());
+        //代表代码沙箱错误
+        executeCodeResponse.setStatus(2);
+        executeCodeResponse.setJudgeInfo(new JudgeInfo());
+        return executeCodeResponse;
     }
 }
